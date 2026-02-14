c7a3d2d00553fca5b7a81035cbd3b73552adf49d
33Lakshay <lakshaykhatri007@gmail.com>
2026-02-14 10:02:10 +0530

    ENV added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { UsersModule } from './users/users.module';
 import { LeadsModule } from './leads/leads.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Lakshay@1',
-      database: 'crm',
-      autoLoadEntities: true,
-      synchronize: true,
-      // logging: true,
+    ConfigModule.forRoot({          //Reads .env file, loads values in provcess.env and makes them available through ConfigService
+      isGlobal: true,
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({            //Build and return the TypeORM config object using ConfigService.
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
     UsersModule,
     LeadsModule,
   ],
